b39fa9ce664eea8bb6032be7d692b34ad95e31fa
Zhanerke <szhanerz@gmail.com>
2022-11-18 19:51:23 -0800

    added tests p1

--- src/test/model/OpportunityListTest.java
+++ src/test/model/OpportunityListTest.java
@@ -1,13 +1,53 @@
 package model;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static model.Availability.*;
+import static model.OpportunityType.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OpportunityListTest {
     private OpportunityList testOpportunityList;
 
     @BeforeEach
     void setup(){
-        testOpportunityList = new OpportunityList();
+        testOpportunityList = new OpportunityList("My opportunity list");
     }
 
+    @Test
+    void testSelectOp(){
+        OpportunityPost op1 = new OpportunityPost("Need a partner",
+                designTeam,
+                new Date(2022-1900, 9, 27),
+                expired);
+        OpportunityPost op2 = new OpportunityPost("Need a volunteer",
+                volunteering,
+                new Date(2022-1900, 11, 27),
+                available);
+        OpportunityPost op3 = new OpportunityPost("Need an intern",
+                internship,
+                new Date(2023-1900, 9, 27),
+                available);
+        testOpportunityList.addOpp(op1);
+        testOpportunityList.addOpp(op2);
+        testOpportunityList.addOpp(op3);
+        assertEquals(op1, testOpportunityList.selectOpp(1));
+        assertEquals(op2, testOpportunityList.selectOpp(2));
+        assertEquals(op3, testOpportunityList.selectOpp(3));
+    }
+
+    @Test
+    void testPrintOpportunities(){
+        OpportunityPost op1 = new OpportunityPost("Need a partner",
+                designTeam,
+                new Date(2022-1900, 9, 27),
+                expired);
+        testOpportunityList.addOpp(op1);
+        //assertEquals("1 : Need a partner ");
+
+
+    }
 }
--- src/test/model/OpportunityPostTest.java
+++ src/test/model/OpportunityPostTest.java
@@ -1,33 +1,39 @@
 package model;
 
-import model.OpportunityPost;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import static model.Availability.*;
+import static model.OpportunityType.internship;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class OpportunityPostTest {
     private OpportunityPost testOpportunityPost;
 
     @BeforeEach
     void runBefore(){
-        testOpportunityPost = new OpportunityPost("Need a partner");
+        testOpportunityPost = new OpportunityPost("Need a partner", internship,
+                new Date(2022-1900, 9,27), available);
     }
 
     @Test
     void testConstructor(){
         assertEquals("Need a partner", testOpportunityPost.getPostName());
-        assertEquals("internship", testOpportunityPost.getOpportunityType());
-        assertEquals("Thu Oct 27 00:00:00 PDT 2022 ", testOpportunityPost.getDueDate());
-        assertEquals("available", testOpportunityPost.getStatus());
+        assertEquals(internship, testOpportunityPost.getOpportunityType());
+        assertEquals(2022-1900, testOpportunityPost.getDueDate().getYear());
+        assertEquals(Calendar.OCTOBER, testOpportunityPost.getDueDate().getMonth());
+        assertEquals(27, testOpportunityPost.getDueDate().getDate());
+        assertEquals(available, testOpportunityPost.getStatus());
     }
 
     @Test
     void testToString() {
-        assertTrue( testOpportunityPost.toString().contains("Name: Need a partner, " +
-                        "Type:internship, Thu Oct 27 00:00:00 PDT 2022, Status: available"));
+        assertTrue(testOpportunityPost.toString().contains("Name: Need a partner, " +
+                "Type: internship, " +
+                "Date: Thu Oct 27 00:00:00 PDT 2022, Status: available"));
     }
-}
-
 }
